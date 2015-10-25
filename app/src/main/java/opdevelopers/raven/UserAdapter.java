3509b8ff05a2846b69715e9658a4cacae6710bc7
Eduardo Ibáñez <eduiba94@gmail.com>
2015-10-25 22:56:54 +0100

    cambios en UserAdapter.java

--- app/src/main/java/opdevelopers/raven/UserAdapter.java
+++ app/src/main/java/opdevelopers/raven/UserAdapter.java
@@ -1,53 +1,120 @@
 package opdevelopers.raven;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.util.EntityUtils;
+import android.os.AsyncTask;
+import android.os.StrictMode;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.net.ssl.HttpsURLConnection;
 
 /**
  * Created by Eduardo on 15/10/2015.
  */
-public class UserAdapter {
+public class UserAdapter extends AsyncTask<Void, Void, Void> {
 
-    HttpClient httpClient = null;
-    HttpPost request = null;
+    HttpURLConnection conn = null;
+    HashMap<String, String> postDataParams = null;
+    String responseBody = "";
 
     public UserAdapter() {
-        httpClient = HttpClientBuilder.create().build();
-        request = new HttpPost("mongodb://192.168.1.108/raven");
+        try {
+            URL url = new URL("http://raven-sirbagus.rhcloud.com/createUser");
+            conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(15000);
+            conn.setConnectTimeout(15000);
+            conn.setRequestMethod("POST");
+            conn.setDoInput(true);
+            conn.setDoOutput(true);
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public boolean enviarPeticion(String nombre, String apellido, String email, String anyoNacimiento,
                                   String telefono, String infoMedica, String residencia, String contrasenya,
                                   String nombreContacto, String apellidoContacto, String telefonoContacto) {
-        try {
-            String details = "details={\"tlf\":\"" + telefono + "\",\"email\":\"" + email + "\","
-                    + "\"nombre\":\"" + nombre + "\",\"apellido\":\"" + apellido + "\","
-                    + "\"pass\":\"" + contrasenya + "\",\"nacimiento\":\"" + anyoNacimiento + "\","
-                    + "\"info\":\"" + infoMedica + "\",\"residencia\":\"" + residencia + "\","
-                    + "\"contacto\": { \"nombre\":\"" + nombreContacto + "\",\"apellido\":\"" + apellidoContacto + "\","
-                    + "\"tlf\":\"" + telefonoContacto + "\" } }";
-            StringEntity params = new StringEntity(details);
-            request.addHeader("Content-Type", "application/x-www-form-urlencoded");
-            request.setEntity(params);
-            HttpResponse response = httpClient.execute(request);
-            String responseBody = EntityUtils.toString(response.getEntity());
+        postDataParams = new HashMap<>();
+        postDataParams.put("tlf", telefono);
+        postDataParams.put("email", email);
+        postDataParams.put("pass", contrasenya);
+        postDataParams.put("nombre", nombre);
+        postDataParams.put("apellido", apellido);
+        postDataParams.put("info", infoMedica);
+        postDataParams.put("residencia", residencia);
+        postDataParams.put("nacimiento", anyoNacimiento);
+        postDataParams.put("contactoNombre", nombreContacto);
+        postDataParams.put("contactoApellido", apellidoContacto);
+        postDataParams.put("contactoTelefono", telefonoContacto);
 
-            if (responseBody.contains("200")) {
-                return true;
-            }
-            else if (responseBody.contains("400")) {
-                return false;
-            }
-            else {
-                return false;
-            }
+        doInBackground();
+
+        if (responseBody.contains("200")) {
+            return true;
         }
-        catch (Exception ex) {
+        else if (responseBody.contains("400")) {
             return false;
         }
+        else {
+            return false;
+        }
+    }
+
+    @Override
+    protected Void doInBackground(Void... params) {
+        try {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+
+            OutputStream os = conn.getOutputStream();
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os, "UTF-8"));
+            writer.write(getPostDataString(postDataParams));
+
+            writer.flush();
+            writer.close();
+            os.close();
+            int responseCode = conn.getResponseCode();
+
+            if (responseCode == HttpsURLConnection.HTTP_OK) {
+                String line;
+                responseBody = "";
+                BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                while ((line = br.readLine()) != null) {
+                    responseBody += line;
+                }
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private String getPostDataString(HashMap<String, String> params) throws UnsupportedEncodingException {
+        StringBuilder result = new StringBuilder();
+        boolean first = true;
+        for (Map.Entry<String, String> entry: params.entrySet()) {
+            if (first) {
+                first = false;
+            }
+            else {
+                result.append("&");
+            }
+            result.append(URLEncoder.encode(entry.getKey(), "UTF-8"));
+            result.append("=");
+            result.append(URLEncoder.encode(entry.getValue(), "UTF-8"));
+        }
+        return result.toString();
     }
 }
