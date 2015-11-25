35fca393728168679646b2b2ecbe5c938b86a06b
Eduardo Ibáñez <eduiba94@gmail.com>
2015-11-25 22:11:23 +0100

    mejorar cobertura de la clase User.java

--- app/src/main/java/opdevelopers/raven/User.java
+++ app/src/main/java/opdevelopers/raven/User.java
@@ -134,87 +134,204 @@ public class User {
         return nombre;
     }
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
+    public void setNombre(String nombre) throws ErrorException {
+        if (nombre == null || nombre.equals("")) {
+            throw new ErrorException();
+        }
+        else {
+            this.nombre = nombre;
+        }
     }
 
     public String getApellido() {
         return apellido;
     }
 
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
+    public void setApellido(String apellido) throws ErrorException {
+        if (apellido == null || apellido.equals("")) {
+            throw new ErrorException();
+        }
+        else {
+            this.apellido = apellido;
+        }
     }
 
     public String getEmail() {
         return email;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
+    public void setEmail(String email) throws ErrorException {
+        if (email == null || email.equals("") || !email.contains("@") || !email.contains(".")) {
+            throw new ErrorException();
+        }
+        else {
+            this.email = email;
+        }
     }
 
     public String getAnyoNacimiento() {
         return anyoNacimiento;
     }
 
-    public void setAnyoNacimiento(String anyoNacimiento) {
-        this.anyoNacimiento = anyoNacimiento;
+    public void setAnyoNacimiento(String anyoNacimiento) throws ErrorException {
+        if (anyoNacimiento == null || anyoNacimiento.equals("") || anyoNacimiento.length() != 4) {
+            throw new ErrorException();
+        }
+        else {
+            try {
+                int anyo = Integer.parseInt(anyoNacimiento);
+                if (anyo < 0) {
+                    throw new ErrorException();
+                }
+            }
+            catch (NumberFormatException e) {
+                throw new ErrorException();
+            }
+            this.anyoNacimiento = anyoNacimiento;
+        }
     }
 
     public String getTelefono() {
         return telefono;
     }
 
-    public void setTelefono(String telefono) {
-        this.telefono = telefono;
+    public void setTelefono(String telefono) throws ErrorException {
+        if (telefono == null || telefono.equals("")) {
+            throw new ErrorException();
+        }
+        else {
+            try {
+                int tlf = Integer.parseInt(telefono);
+                if (tlf < 0) {
+                    throw new ErrorException();
+                }
+            }
+            catch (NumberFormatException e) {
+                throw new ErrorException();
+            }
+            this.telefono = telefono;
+        }
     }
 
     public String getInfoMedica() {
         return infoMedica;
     }
 
-    public void setInfoMedica(String infoMedica) {
-        this.infoMedica = infoMedica;
+    public void setInfoMedica(String infoMedica) throws ErrorException {
+        if (infoMedica == null || infoMedica.equals("")) {
+            throw new ErrorException();
+        }
+        else {
+            this.infoMedica = infoMedica;
+        }
     }
 
     public String getResidencia() {
         return residencia;
     }
 
-    public void setResidencia(String residencia) {
-        this.residencia = residencia;
+    public void setResidencia(String residencia) throws ErrorException {
+        if (residencia == null || residencia.equals("")) {
+            throw new ErrorException();
+        } else {
+            this.residencia = residencia;
+        }
     }
 
     public String getContrasenya() {
         return contrasenya;
     }
 
-    public void setContrasenya(String contrasenya) {
-        this.contrasenya = contrasenya;
+    public void setContrasenya(String contrasenya) throws ErrorException {
+        if (contrasenya == null || contrasenya.equals("")) {
+            throw new ErrorException();
+        }
+        else {
+            this.contrasenya = contrasenya;
+        }
     }
 
     public String getNombreContacto() {
         return nombreContacto;
     }
 
-    public void setNombreContacto(String nombreContacto) {
-        this.nombreContacto = nombreContacto;
+    public void setNombreContacto(String nombreContacto) throws ErrorException {
+        if (nombreContacto == null || nombreContacto.equals("")) {
+            throw new ErrorException();
+        }
+        else {
+            this.nombreContacto = nombreContacto;
+        }
     }
 
     public String getApellidoContacto() {
         return apellidoContacto;
     }
 
-    public void setApellidoContacto(String apellidoContacto) {
-        this.apellidoContacto = apellidoContacto;
+    public void setApellidoContacto(String apellidoContacto) throws ErrorException {
+        if (apellidoContacto == null || apellidoContacto.equals("")) {
+            throw new ErrorException();
+        }
+        else {
+            this.apellidoContacto = apellidoContacto;
+        }
     }
 
     public String getTelefonoContacto() {
         return telefonoContacto;
     }
 
-    public void setTelefonoContacto(String telefonoContacto) {
-        this.telefonoContacto = telefonoContacto;
+    public void setTelefonoContacto(String telefonoContacto) throws ErrorException {
+        if (telefonoContacto == null || telefonoContacto.equals("")) {
+            throw new ErrorException();
+        }
+        else {
+            try {
+                int tlfContacto = Integer.parseInt(telefonoContacto);
+                if (tlfContacto < 0) {
+                    throw new ErrorException();
+                }
+            }
+            catch (NumberFormatException e) {
+                throw new ErrorException();
+            }
+            this.telefonoContacto = telefonoContacto;
+        }
+    }
+
+    @Override
+    public String toString() {
+        String cadena = "INFO USUARIO\n";
+        cadena += "===========\n";
+        if (this.nombre != null) {
+            cadena += "nombre -> " + this.nombre + "\n";
+        }
+        if (this.apellido != null) {
+            cadena += "apellido -> " + this.apellido + "\n";
+        }
+        cadena += "email -> " + this.email + "\n";
+        if (this.anyoNacimiento != null) {
+            cadena += "anyoNacimiento -> " + this.anyoNacimiento + "\n";
+        }
+        if (this.telefono != null) {
+            cadena += "telefono -> " + this.telefono + "\n";
+        }
+        if (this.infoMedica != null) {
+            cadena += "infoMedica -> " + this.infoMedica + "\n";
+        }
+        if (this.residencia != null) {
+            cadena += "residencia -> " + this.residencia + "\n";
+        }
+        cadena += "contrasenya -> " + this.contrasenya + "\n";
+        if (this.nombreContacto != null) {
+            cadena += "nombreContacto -> " + this.nombreContacto + "\n";
+        }
+        if (this.apellidoContacto != null) {
+            cadena += "apellidoContacto -> " + this.apellidoContacto + "\n";
+        }
+        if (this.telefonoContacto != null) {
+            cadena += "telefonoContacto -> " + this.telefonoContacto + "\n";
+        }
+        return cadena;
     }
 }
--- app/src/test/java/opdevelopers/raven/UserTest.java
+++ app/src/test/java/opdevelopers/raven/UserTest.java
@@ -39,6 +39,15 @@ public class UserTest {
         User usuario = new User("correo@example.com", "");
     }
 
+    @Test
+    public void testCorreoContrasenyaCorrecto() throws ErrorException {
+        User usuario = new User("correo@example.com", "contrasenya");
+        User prueba = new User("prueba@test.com", "password");
+        prueba.setEmail("correo@example.com");
+        prueba.setContrasenya("contrasenya");
+        assertEquals(usuario.toString(), prueba.toString());
+    }
+
     @Test(expected=ErrorException.class)
     public void testNombreNull() throws ErrorException {
         User usuario = new User(null, "Apellido", "correo@example.com", "1994", "976543210",
@@ -223,5 +232,289 @@ public class UserTest {
     public void testTelefonoContactoEntero() throws ErrorException {
         User usuario = new User("Nombre", "Apellido", "correo@example.com", "1994", "976543210",
                 "Sano", "Zaragoza", "prueba", "NombreContacto", "ApellidoContacto", "Seis siete ocho");
+    }
+
+    @Test
+    public void testUsuarioCorrecto1() throws ErrorException {
+        User usuario = new User("Nombre", "Apellido", "correo@example.com", "1994", "976543210",
+                "Sano", "Zaragoza", "prueba", "NombreContacto", "ApellidoContacto", "678654321");
+        User prueba = new User("Name", "Surname", "prueba@test.com", "1989", "976012345",
+                "Enfermo", "Teruel", "test", "Name2", "Surname2", "678123456");
+        prueba.setNombre("Nombre");
+        prueba.setApellido("Apellido");
+        prueba.setEmail("correo@example.com");
+        prueba.setAnyoNacimiento("1994");
+        prueba.setTelefono("976543210");
+        prueba.setInfoMedica("Sano");
+        prueba.setResidencia("Zaragoza");
+        prueba.setContrasenya("prueba");
+        prueba.setNombreContacto("NombreContacto");
+        prueba.setApellidoContacto("ApellidoContacto");
+        prueba.setTelefonoContacto("678654321");
+        assertEquals(usuario.toString(), prueba.toString());
+    }
+
+    @Test
+    public void testUsuarioCorrecto2() throws ErrorException {
+        User usuario = new User("Nombre", "Apellido", "correo@example.com", "1994", "976543210",
+                "Sano", "Zaragoza", "prueba", "NombreContacto", "ApellidoContacto", "678654321");
+        User prueba = new User(usuario.getNombre(), usuario.getApellido(), usuario.getEmail(),
+                usuario.getAnyoNacimiento(), usuario.getTelefono(), usuario.getInfoMedica(),
+                usuario.getResidencia(), usuario.getContrasenya(), usuario.getNombreContacto(),
+                usuario.getApellidoContacto(), usuario.getTelefonoContacto());
+        assertEquals(usuario.toString(), prueba.toString());
+    }
+
+    @Test(expected=ErrorException.class)
+    public void testSetCorreoNull() throws ErrorException {
+        User usuario = new User("correo@example.com", "contrasenya");
+        usuario.setEmail(null);
+    }
+
+    @Test(expected=ErrorException.class)
+    public void testSetCorreoVacio() throws ErrorException {
+        User usuario = new User("correo@example.com", "contrasenya");
+        usuario.setEmail("");
+    }
+
+    @Test(expected=ErrorException.class)
+    public void testSetCorreoNoArroba() throws ErrorException {
+        User usuario = new User("correo@example.com", "contrasenya");
+        usuario.setEmail("correoexample.com");
+    }
+
+    @Test(expected=ErrorException.class)
+    public void testSetCorreoNoPunto() throws ErrorException {
+        User usuario = new User("correo@example.com", "contrasenya");
+        usuario.setEmail("correo@examplecom");
+    }
+
+    @Test(expected=ErrorException.class)
+    public void testSetContrasenyaNull() throws ErrorException {
+        User usuario = new User("correo@example.com", "contrasenya");
+        usuario.setContrasenya(null);
+    }
+
+    @Test(expected=ErrorException.class)
+    public void testSetContrasenyaVacio() throws ErrorException {
+        User usuario = new User("correo@example.com", "contrasenya");
+        usuario.setContrasenya("");
+    }
+
+    @Test(expected=ErrorException.class)
+    public void testSetNombreNull() throws ErrorException {
+        User usuario = new User("Nombre", "Apellido", "correo@example.com", "1994", "976543210",
+                "Sano", "Zaragoza", "prueba", "NombreContacto", "ApellidoContacto", "678654321");
+        usuario.setNombre(null);
+    }
+
+    @Test(expected=ErrorException.class)
+    public void testSetNombreVacio() throws ErrorException {
+        User usuario = new User("Nombre", "Apellido", "correo@example.com", "1994", "976543210",
+                "Sano", "Zaragoza", "prueba", "NombreContacto", "ApellidoContacto", "678654321");
+        usuario.setNombre("");
+    }
+
+    @Test(expected=ErrorException.class)
+    public void testSetApellidoNull() throws ErrorException {
+        User usuario = new User("Nombre", "Apellido", "correo@example.com", "1994", "976543210",
+                "Sano", "Zaragoza", "prueba", "NombreContacto", "ApellidoContacto", "678654321");
+        usuario.setApellido(null);
+    }
+
+    @Test(expected=ErrorException.class)
+    public void testSetApellidoVacio() throws ErrorException {
+        User usuario = new User("Nombre", "Apellido", "correo@example.com", "1994", "976543210",
+                "Sano", "Zaragoza", "prueba", "NombreContacto", "ApellidoContacto", "678654321");
+        usuario.setApellido("");
+    }
+
+    @Test(expected=ErrorException.class)
+    public void testSetEmailNull() throws ErrorException {
+        User usuario = new User("Nombre", "Apellido", "correo@example.com", "1994", "976543210",
+                "Sano", "Zaragoza", "prueba", "NombreContacto", "ApellidoContacto", "678654321");
+        usuario.setEmail(null);
+    }
+
+    @Test(expected=ErrorException.class)
+    public void testSetEmailVacio() throws ErrorException {
+        User usuario = new User("Nombre", "Apellido", "correo@example.com", "1994", "976543210",
+                "Sano", "Zaragoza", "prueba", "NombreContacto", "ApellidoContacto", "678654321");
+        usuario.setEmail("");
+    }
+
+    @Test(expected=ErrorException.class)
+    public void testSetEmailNoArroba() throws ErrorException {
+        User usuario = new User("Nombre", "Apellido", "correo@example.com", "1994", "976543210",
+                "Sano", "Zaragoza", "prueba", "NombreContacto", "ApellidoContacto", "678654321");
+        usuario.setEmail("correoexample.com");
+    }
+
+    @Test(expected=ErrorException.class)
+    public void testSetEmailNoPunto() throws ErrorException {
+        User usuario = new User("Nombre", "Apellido", "correo@example.com", "1994", "976543210",
+                "Sano", "Zaragoza", "prueba", "NombreContacto", "ApellidoContacto", "678654321");
+        usuario.setEmail("correo@examplecom");
+    }
+
+    @Test(expected=ErrorException.class)
+    public void testSetAnyoNull() throws ErrorException {
+        User usuario = new User("Nombre", "Apellido", "correo@example.com", "1994", "976543210",
+                "Sano", "Zaragoza", "prueba", "NombreContacto", "ApellidoContacto", "678654321");
+        usuario.setAnyoNacimiento(null);
+    }
+
+    @Test(expected=ErrorException.class)
+    public void testSetAnyoVacio() throws ErrorException {
+        User usuario = new User("Nombre", "Apellido", "correo@example.com", "1994", "976543210",
+                "Sano", "Zaragoza", "prueba", "NombreContacto", "ApellidoContacto", "678654321");
+        usuario.setAnyoNacimiento("");
+    }
+
+    @Test(expected=ErrorException.class)
+    public void testSetAnyoCuatroCifras() throws ErrorException {
+        User usuario = new User("Nombre", "Apellido", "correo@example.com", "1994", "976543210",
+                "Sano", "Zaragoza", "prueba", "NombreContacto", "ApellidoContacto", "678654321");
+        usuario.setAnyoNacimiento("19994");
+    }
+
+    @Test(expected=ErrorException.class)
+    public void testSetAnyoPositivo() throws ErrorException {
+        User usuario = new User("Nombre", "Apellido", "correo@example.com", "1994", "976543210",
+                "Sano", "Zaragoza", "prueba", "NombreContacto", "ApellidoContacto", "678654321");
+        usuario.setAnyoNacimiento("-1994");
+    }
+
+    @Test(expected=ErrorException.class)
+    public void testSetAnyoEntero() throws ErrorException {
+        User usuario = new User("Nombre", "Apellido", "correo@example.com", "1994", "976543210",
+                "Sano", "Zaragoza", "prueba", "NombreContacto", "ApellidoContacto", "678654321");
+        usuario.setAnyoNacimiento("Dos mil quince");
+    }
+
+    @Test(expected=ErrorException.class)
+    public void testSetTelefonoNull() throws ErrorException {
+        User usuario = new User("Nombre", "Apellido", "correo@example.com", "1994", "976543210",
+                "Sano", "Zaragoza", "prueba", "NombreContacto", "ApellidoContacto", "678654321");
+        usuario.setTelefono(null);
+    }
+
+    @Test(expected=ErrorException.class)
+    public void testSetTelefonoVacio() throws ErrorException {
+        User usuario = new User("Nombre", "Apellido", "correo@example.com", "1994", "976543210",
+                "Sano", "Zaragoza", "prueba", "NombreContacto", "ApellidoContacto", "678654321");
+        usuario.setTelefono("");
+    }
+
+    @Test(expected=ErrorException.class)
+    public void testSetTelefonoPositivo() throws ErrorException {
+        User usuario = new User("Nombre", "Apellido", "correo@example.com", "1994", "976543210",
+                "Sano", "Zaragoza", "prueba", "NombreContacto", "ApellidoContacto", "678654321");
+        usuario.setTelefono("-976543210");
+    }
+
+    @Test(expected=ErrorException.class)
+    public void testSetTelefonoEntero() throws ErrorException {
+        User usuario = new User("Nombre", "Apellido", "correo@example.com", "1994", "976543210",
+                "Sano", "Zaragoza", "prueba", "NombreContacto", "ApellidoContacto", "678654321");
+        usuario.setTelefono("Nueve siete seis");
+    }
+
+    @Test(expected=ErrorException.class)
+    public void testSetInfoMedicaNull() throws ErrorException {
+        User usuario = new User("Nombre", "Apellido", "correo@example.com", "1994", "976543210",
+                "Sano", "Zaragoza", "prueba", "NombreContacto", "ApellidoContacto", "678654321");
+        usuario.setInfoMedica(null);
+    }
+
+    @Test(expected=ErrorException.class)
+    public void testSetInfoMedicaVacio() throws ErrorException {
+        User usuario = new User("Nombre", "Apellido", "correo@example.com", "1994", "976543210",
+                "Sano", "Zaragoza", "prueba", "NombreContacto", "ApellidoContacto", "678654321");
+        usuario.setInfoMedica("");
+    }
+
+    @Test(expected=ErrorException.class)
+    public void testSetResidenciaNull() throws ErrorException {
+        User usuario = new User("Nombre", "Apellido", "correo@example.com", "1994", "976543210",
+                "Sano", "Zaragoza", "prueba", "NombreContacto", "ApellidoContacto", "678654321");
+        usuario.setResidencia(null);
+    }
+
+    @Test(expected=ErrorException.class)
+    public void testSetResidenciaVacio() throws ErrorException {
+        User usuario = new User("Nombre", "Apellido", "correo@example.com", "1994", "976543210",
+                "Sano", "Zaragoza", "prueba", "NombreContacto", "ApellidoContacto", "678654321");
+        usuario.setResidencia("");
+    }
+
+    @Test(expected=ErrorException.class)
+    public void testSetPasswordNull() throws ErrorException {
+        User usuario = new User("Nombre", "Apellido", "correo@example.com", "1994", "976543210",
+                "Sano", "Zaragoza", "prueba", "NombreContacto", "ApellidoContacto", "678654321");
+        usuario.setContrasenya(null);
+    }
+
+    @Test(expected=ErrorException.class)
+    public void testSetPasswordVacio() throws ErrorException {
+        User usuario = new User("Nombre", "Apellido", "correo@example.com", "1994", "976543210",
+                "Sano", "Zaragoza", "prueba", "NombreContacto", "ApellidoContacto", "678654321");
+        usuario.setContrasenya("");
+    }
+
+    @Test(expected=ErrorException.class)
+    public void testSetNombreContactoNull() throws ErrorException {
+        User usuario = new User("Nombre", "Apellido", "correo@example.com", "1994", "976543210",
+                "Sano", "Zaragoza", "prueba", "NombreContacto", "ApellidoContacto", "678654321");
+        usuario.setNombreContacto(null);
+    }
+
+    @Test(expected=ErrorException.class)
+    public void testSetNombreContactoVacio() throws ErrorException {
+        User usuario = new User("Nombre", "Apellido", "correo@example.com", "1994", "976543210",
+                "Sano", "Zaragoza", "prueba", "NombreContacto", "ApellidoContacto", "678654321");
+        usuario.setNombreContacto("");
+    }
+
+    @Test(expected=ErrorException.class)
+    public void testSetApellidoContactoNull() throws ErrorException {
+        User usuario = new User("Nombre", "Apellido", "correo@example.com", "1994", "976543210",
+                "Sano", "Zaragoza", "prueba", "NombreContacto", "ApellidoContacto", "678654321");
+        usuario.setApellido(null);
+    }
+
+    @Test(expected=ErrorException.class)
+    public void testSetApellidoContactoVacio() throws ErrorException {
+        User usuario = new User("Nombre", "Apellido", "correo@example.com", "1994", "976543210",
+                "Sano", "Zaragoza", "prueba", "NombreContacto", "ApellidoContacto", "678654321");
+        usuario.setApellido("");
+    }
+
+    @Test(expected=ErrorException.class)
+    public void testSetTelefonoContactoNull() throws ErrorException {
+        User usuario = new User("Nombre", "Apellido", "correo@example.com", "1994", "976543210",
+                "Sano", "Zaragoza", "prueba", "NombreContacto", "ApellidoContacto", "678654321");
+        usuario.setTelefonoContacto(null);
+    }
+
+    @Test(expected=ErrorException.class)
+    public void testSetTelefonoContactoVacio() throws ErrorException {
+        User usuario = new User("Nombre", "Apellido", "correo@example.com", "1994", "976543210",
+                "Sano", "Zaragoza", "prueba", "NombreContacto", "ApellidoContacto", "678654321");
+        usuario.setTelefonoContacto("");
+    }
+
+    @Test(expected=ErrorException.class)
+    public void testSetTelefonoContactoPositivo() throws ErrorException {
+        User usuario = new User("Nombre", "Apellido", "correo@example.com", "1994", "976543210",
+                "Sano", "Zaragoza", "prueba", "NombreContacto", "ApellidoContacto", "678654321");
+        usuario.setTelefonoContacto("-678654321");
+    }
+
+    @Test(expected=ErrorException.class)
+    public void testSetTelefonoContactoEntero() throws ErrorException {
+        User usuario = new User("Nombre", "Apellido", "correo@example.com", "1994", "976543210",
+                "Sano", "Zaragoza", "prueba", "NombreContacto", "ApellidoContacto", "678654321");
+        usuario.setTelefonoContacto("Seis siete ocho");
     }
 }
