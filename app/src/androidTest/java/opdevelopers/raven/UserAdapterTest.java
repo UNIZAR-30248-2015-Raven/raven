140cf76565fbdf648868a88f3a6b12277220585e
Daniel Uroz <uzetta27@hotmail.com>
2016-01-13 13:34:29 +0100

    Test borrar usuario

--- app/src/androidTest/java/opdevelopers/raven/UserAdapterTest.java
+++ app/src/androidTest/java/opdevelopers/raven/UserAdapterTest.java
@@ -9,21 +9,36 @@ import java.util.concurrent.TimeUnit;
  * Created by Eduardo on 01/12/2015.
  */
 public class UserAdapterTest extends InstrumentationTestCase {
+    public static boolean peticionCreacionAceptada = false;
+    public static boolean peticionInicioSesionAceptada = false;
+    public static boolean peticionBorrarAceptada = false;
 
-    public static boolean peticionUsuarioAceptada1 = false;
-    public static boolean peticionUsuarioAceptada2 = false;
 
-    public void testUserAdapterLogin() throws Throwable {
-        final CountDownLatch signalUser1 = new CountDownLatch(1);
-        final CountDownLatch signalUser2 = new CountDownLatch(1);
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
 
+        try {
+            createUser();
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Debe de crear un nuevo usuario
+     *
+     * @throws Throwable
+     */
+    public void createUser() throws Throwable {
+        final CountDownLatch signalUser = new CountDownLatch(1);
 
         final UserAdapter adaptadorUsuarios1 = new UserAdapter(Constants.CREATE_USER, true) {
 
             @Override
             public void onPostExecute() {
                 super.onPostExecute();
-                signalUser1.countDown();
+                signalUser.countDown();
             }
         };
 
@@ -32,42 +47,90 @@ public class UserAdapterTest extends InstrumentationTestCase {
             @Override
             public void run() {
                 try {
-                    User usuario = new User("Nombre", "Apellido", "ejemplo32@example.com", "1994", "776131313",
+                    User usuario = new User("Nombre", "Apellido", "jjj@jjj.com", "1994", "776131311",
                             "Sano", "Zaragoza", "prueba", "NombreContacto", "ApellidoContacto", "678654320");
-                    UserAdapterTest.peticionUsuarioAceptada1 = adaptadorUsuarios1.enviarPeticionRegistrar(usuario);
+                    UserAdapterTest.peticionCreacionAceptada = adaptadorUsuarios1.enviarPeticionRegistrar(usuario);
                 } catch (ErrorException e) {
                     e.printStackTrace();
                 }
             }
         });
+        
+        assertTrue(peticionCreacionAceptada);
+    }
 
-        signalUser1.await(35, TimeUnit.SECONDS);
+
+    /**
+     * Debe iniciar sesión de un usuario
+     *
+     * @throws Throwable
+     */
+    public void testLoginUser() throws Throwable {
+        final CountDownLatch signalUser = new CountDownLatch(1);
 
         final UserAdapter adaptadorUsuarios2 = new UserAdapter(Constants.CREATE_USER, false) {
 
             @Override
             public void onPostExecute() {
                 super.onPostExecute();
-                signalUser2.countDown();
+                signalUser.countDown();
             }
         };
 
         runTestOnUiThread(new Runnable() {
-
             @Override
             public void run() {
                 try {
-                    User usuario = new User("ejemplo32@example.com", "prueba");
-                    UserAdapterTest.peticionUsuarioAceptada2 = adaptadorUsuarios2.enviarPeticionSesion(usuario);
+                    User usuario = new User("jjj@jjj.com", "prueba");
+                    UserAdapterTest.peticionInicioSesionAceptada = adaptadorUsuarios2.enviarPeticionSesion(usuario);
                 } catch (ErrorException e) {
                     e.printStackTrace();
                 }
             }
         });
 
-        signalUser2.await(35, TimeUnit.SECONDS);
+        assertTrue(peticionInicioSesionAceptada);
+    }
 
-        assertTrue(peticionUsuarioAceptada1);
-        assertTrue(peticionUsuarioAceptada2);
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+
+        try {
+            deleteUser();
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+    /**
+     * Debe de borrar un usuario
+     *
+     * @throws Throwable
+     */
+    public void deleteUser() throws Throwable {
+        final CountDownLatch signalUser = new CountDownLatch(1);
+
+        final UserAdapter adaptadorUsuarios3 = new UserAdapter(Constants.DELETE_USER, true) {
+
+            @Override
+            public void onPostExecute() {
+                super.onPostExecute();
+                signalUser.countDown();
+            }
+        };
+
+        runTestOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                UserAdapterTest.peticionBorrarAceptada = adaptadorUsuarios3
+                        .enviarPeticionBorrar("jjj@jjj.com", "prueba");
+            }
+        });
+
+        assertTrue(peticionBorrarAceptada);
     }
 }
